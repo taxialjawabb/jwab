11d179716d2e07a9e8f4113c50904558215e0ceb
ahmedmarei0 <ahmedmarei1996@gmail.com>
2022-04-24 02:21:27 +0300

    modify file exist helper function

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -87,8 +87,6 @@ class Controller extends BaseController
     public function show_pdf()
     {
         $path =  asset('public/'.request()->url);
-        return $path;
-
         if(File::exists($path)){
             return response()->file($path);
         }else{
